4e98ba3168964cf669056d1aefd657d213a7a57c
khattaby <aymanibrahim678@gmail.com>
2024-11-08 14:37:56 +0200

    search bar html &css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from './Logo'
+import { GrSearch } from "react-icons/gr";
+
 
 const Header = () => {
   return (
@@ -11,8 +13,11 @@ const Header = () => {
         </div>
 
 
-        <div>
-          search
+        <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
+          <input type='text' placeholder='Search product' className='w-full outline-none' />
+          <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
+            <GrSearch />
+          </div>
         </div>
 
 
