0a3df6373e02920f23120732d38f84c952c46abb
khattaby <aymanibrahim678@gmail.com>
2024-11-08 13:28:20 +0200

    routes - home page & header & footer components creating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='text-red-600'>
-      mern app
-    </div>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 }
 
