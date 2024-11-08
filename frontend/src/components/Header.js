e0f1d4cbc07da39ea6c6ad15e921bcd28b0277d4
khattaby <aymanibrahim678@gmail.com>
2024-11-08 13:53:16 +0200

    asset file and starting the navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Logo from './Logo'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <header className='h-16 shadow-md'>
+      <div className='h-full container mx-auto flex items-center px-4 justify-between'>
+
+        <div className=''>
+          <Logo w={90} h={50} />
+        </div>
+
+
+        <div>
+          search
+        </div>
+
+
+        <div>
+          user icon & card
+        </div>
+
+      </div>
+    </header>
   )
 }
 
